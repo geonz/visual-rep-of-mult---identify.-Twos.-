ac937ec2a9bfad8d31eff6c3817351650c6c63fc
xiousgeonz <siouxgeonz@gmail.com>
2019-11-20 23:22:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var factNum=1;
+
+
+function newFact()
+{
+  p1.innerHTML="fact number is: " + factNum;
+ 
+}
+
+function checkName(picNum)
+{
+
+}
+function checkAnswer()
+{
+// check it and also put in the other space "2 x N = "
+}
+
+newFact();
+// Here we will have a function to figure out *what* the answers are becuase 
+// we will have different scenarios w/ this setup perhaps. 
+// no, silly you:   MAKE the answers.  The same way you did the questions in the other one!!!   
